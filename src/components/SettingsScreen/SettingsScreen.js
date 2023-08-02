d85f0750fbdc6d16f09dd05648be883045226bcf
naydenangelov1206 <naydenangelov1206@gmail.com>
2023-08-02 20:37:08 +0300

    added styling to the Settings Screen component

--- src/components/SettingsScreen/SettingsScreen.js
+++ src/components/SettingsScreen/SettingsScreen.js
@@ -1,24 +1,34 @@
+import "./SettingsScreen.css";
+
 import SelectOption from "../SelectOption/SelectOption";
 import { categoryOptions, difficultyOptions } from "../../utils/options";
 
 const SettingsScreen = () => {
   return (
-    <div>
-      <button>❌</button>
+    <div className="settingsContainer">
+      <div className="cancelButtonContainer">
+        <button>❌</button>
+      </div>
 
-      <label htmlFor="category">
-        <select name="category" id="category">
-          {categoryOptions.map(c => {
-            return <SelectOption option={c} />;
-          })}
-        </select>
+      <div className="optionsContainer">
+        <label htmlFor="category">
+          Category:
+          <select name="category" id="category">
+            {categoryOptions.map(c => {
+              return <SelectOption option={c} />;
+            })}
+          </select>
+        </label>
 
-        <select name="difficulty" id="difficulty">
-          {difficultyOptions.map(d => {
-            return <SelectOption option={d} />;
-          })}
-        </select>
-      </label>
+        <label htmlFor="difficulty">
+          Difficulty:
+          <select name="difficulty" id="difficulty">
+            {difficultyOptions.map(d => {
+              return <SelectOption option={d} />;
+            })}
+          </select>
+        </label>
+      </div>
     </div>
   );
 };
